fec9c98da02ec19192f13ad9a235488c7545560c
JoanaSilvaa <sjoana014@gmail.com>
2024-03-26 22:02:42 -0300

    testando o nextInt

--- trabalhos/t1/app/src/main/java/engtelecom/rtr/Pcm.java
+++ trabalhos/t1/app/src/main/java/engtelecom/rtr/Pcm.java
@@ -4,18 +4,26 @@ import java.util.Scanner;
 
 public class Pcm {
     private int paq[]= {0011011};
-    private String arquivo;
+    //private String arquivo;
+    private int[] b;
 
     public void openFile(Scanner entrada){
         while(entrada.hasNext()){
-            String linha = entrada.nextLine();
-            //TODO testar entrada.nextByte() - ver o que vai sair com isso
-            //lembrando que tem que ler byte por Byte (8 bits)
-
-            System.out.println(linha);
+            //arquivo = entrada.nextLine();
+            //arquivo = entrada.nextInt();
+            System.out.println(entrada.nextInt());
+            System.out.println("quebra");
+            //System.out.println(entrada.length());
         }
     }
 
+
+//    private void separaByte(){
+//        //int aux = 1;
+//        for (int i = 0; i < arquivo.length(); i++) {
+//            b = arquivo.substring()
+//        }
+//    }
     //fazer um metodo que separe a linha lida em 8 bits
 
 }
