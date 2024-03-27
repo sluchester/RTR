b196cecc8fd55ecb00ae80dbbaf68fd504f1c3ca
JoanaSilvaa <sjoana014@gmail.com>
2024-03-27 20:03:07 -0300

    teste

--- trabalhos/t1/app/src/main/java/engtelecom/rtr/Pcm.java
+++ trabalhos/t1/app/src/main/java/engtelecom/rtr/Pcm.java
@@ -3,27 +3,27 @@ package engtelecom.rtr;
 import java.util.Scanner;
 
 public class Pcm {
-    private int paq[]= {0011011};
-    //private String arquivo;
+    private String paq = "0011011";
+    private String arquivo;
     private int[] b;
 
     public void openFile(Scanner entrada){
         while(entrada.hasNext()){
-            //arquivo = entrada.nextLine();
-            //arquivo = entrada.nextInt();
-            System.out.println(entrada.nextInt());
-            System.out.println("quebra");
-            //System.out.println(entrada.length());
+            arquivo = entrada.nextLine();
         }
+        separaByte();
     }
 
-
-//    private void separaByte(){
-//        //int aux = 1;
-//        for (int i = 0; i < arquivo.length(); i++) {
-//            b = arquivo.substring()
-//        }
-//    }
-    //fazer um metodo que separe a linha lida em 8 bits
-
+    private void separaByte(){
+        int cont=0;
+        char[] var = arquivo.toCharArray();
+        char[] byyte;
+        for (int i = 0; i < var.length; i++) {
+            if (i == 7) {
+                byyte == paq;
+            } else{
+                 byyte = var[i];
+            }
+        }
+    }
 }
