ab4e4fa41a2422120619bc7bdfec063d8e0d5878
Luan de Barros <luanbarrossantos@gmail.com>
2024-03-28 18:17:49 -0300

    testando sugestões do gemini

--- trabalhos/t1/app/src/main/java/engtelecom/rtr/Pcm.java
+++ trabalhos/t1/app/src/main/java/engtelecom/rtr/Pcm.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Pcm {
     private String paq = "0011011";
     private String arquivo;
-    private int[] b;
+    //private int[] b;
 
-    public void openFile(Scanner entrada){
+    /*public void openFile(Scanner entrada){
         while(entrada.hasNext()){
             arquivo = entrada.nextLine();
         }
@@ -24,6 +24,28 @@ public class Pcm {
             } else{
                  byyte = var[i];
             }
+        }
+    }*/
+
+    public void openFile(Scanner entrada){
+        while(entrada.hasNextLine()){
+            arquivo = entrada.nextLine();
+            separaByte();
+        }
+    }
+
+    private void separaByte(){
+        int cont = 0;
+        if(arquivo.contains(paq)){
+            cont++;
+            if(cont == 3){
+                for (int i = (arquivo.indexOf(paq)+7); (i < arquivo.length()) && (i < (arquivo.indexOf(paq)+17)); i++) {
+                    System.out.print(arquivo.charAt(i));
+                }
+                System.out.println();
+            }
+        } else{
+            System.out.println("sequencia não eocntrada");
         }
     }
 }
