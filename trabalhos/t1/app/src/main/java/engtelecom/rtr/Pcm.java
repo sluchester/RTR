8547a9364626792533c9b7fb022be647008af79f
Luan de Barros <luanbarrossantos@gmail.com>
2024-03-29 00:03:54 -0300

    formatted document for receive bits

--- trabalhos/t1/app/src/main/java/engtelecom/rtr/Pcm.java
+++ trabalhos/t1/app/src/main/java/engtelecom/rtr/Pcm.java
@@ -1,51 +1,36 @@
 package engtelecom.rtr;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Pcm {
     private String paq = "0011011";
     private String arquivo;
-    //private int[] b;
-
-    /*public void openFile(Scanner entrada){
-        while(entrada.hasNext()){
-            arquivo = entrada.nextLine();
-        }
-        separaByte();
-    }
-
-    private void separaByte(){
-        int cont=0;
-        char[] var = arquivo.toCharArray();
-        char[] byyte;
-        for (int i = 0; i < var.length; i++) {
-            if (i == 7) {
-                byyte == paq;
-            } else{
-                 byyte = var[i];
-            }
-        }
-    }*/
+    private int[] bits;
 
     public void openFile(Scanner entrada){
         while(entrada.hasNextLine()){
             arquivo = entrada.nextLine();
-            separaByte();
+        }
+        System.out.println(arquivo.length());
+        convert();
+    }
+
+    private void convert(){
+        bits = new int[arquivo.length()];
+
+        for (int i = 0; i < arquivo.length(); i++) {
+            char caractere = arquivo.charAt(i);
+            if(caractere == '0'){
+                bits[i] = '0';
+            } else if (caractere == '1') {
+                bits[i] = '1';
+            } else{
+                System.out.println("caractere invalido");
+            }
         }
     }
 
     private void separaByte(){
-        int cont = 0;
-        if(arquivo.contains(paq)){
-            cont++;
-            if(cont == 3){
-                for (int i = (arquivo.indexOf(paq)+7); (i < arquivo.length()) && (i < (arquivo.indexOf(paq)+17)); i++) {
-                    System.out.print(arquivo.charAt(i));
-                }
-                System.out.println();
-            }
-        } else{
-            System.out.println("sequencia não eocntrada");
-        }
     }
 }
