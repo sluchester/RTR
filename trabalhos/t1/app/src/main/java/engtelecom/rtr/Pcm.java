c8897b1f27c6a999ff88ca90c9dc7e49aff6b114
Luan de Barros <54451444+sluchester@users.noreply.github.com>
2024-04-03 22:05:33 -0300

    Update Pcm.java
    finalizado trabalho

--- trabalhos/t1/app/src/main/java/engtelecom/rtr/Pcm.java
+++ trabalhos/t1/app/src/main/java/engtelecom/rtr/Pcm.java
@@ -6,17 +6,14 @@ import java.util.Scanner;
 public class Pcm {
     private String paq = "10011011";
     private String arquivo;
-    private String[] byyts;
 
     public void openFile(Scanner entrada){
         while(entrada.hasNextLine()) {
             arquivo += entrada.nextLine();
         }
-//        System.out.println(arquivo.length());
-//        System.out.println(arquivo);
     }
 
-    public String formatString(){
+    private String formatString(){
         String formattedString = "";
         for (int c = 0; c < arquivo.length(); c++){
             if (arquivo.charAt(c) == '0'){
@@ -25,48 +22,23 @@ public class Pcm {
                 formattedString += '1';
             }
         }
+//        System.out.println("O arquivo contém " + formattedString.length() + " números binários");
         return formattedString;
     }
-
-    /*public void testEE(){
-        System.out.println(formatString(arquivo));
-    }*/
-    /*private String[] convert(){
-        int[] bits = new int[arquivo.length()];
-        byyts = new String[arquivo.length()];
-        int cont = 0;
-        int byyteIndex = 0;
-
-        for (int i = 0; i < arquivo.length(); i++) {
-            if(cont < 8) {
-                char caractere = arquivo.charAt(i);
-                if (caractere == '0') {
-                    bits[cont] = '0';
-                } else if (caractere == '1') {
-                    bits[cont] = '1';
-                } else {
-                    System.out.println("caractere invalido");
-                }
-                cont++;
-            } else {
-                for (int j = 0; j < bits.length; j++) {
-                    //String str = bits[j];
-                }
-                //byyts[byyteIndex] =
-                cont = 0;
-                byyteIndex++;
-            }
-        }
-        return byyts;
-    }*/
 
     public void findPaq(){
         for (int i = 0; i < formatString().length(); i++) {
             String str = formatString().substring(i, i+8);
-
-            if (str.equals(paq)) System.out.println("Encontrou na posição " + i);
-
-            // System.out.println(str);
+            if((i+8) == formatString().length()){
+                break;
+            } else{
+                if (str.equals(paq)) {
+                    System.out.println("Encontrou PAQ na posição " + i);
+                    if(str.charAt(2) == '1'){
+                        System.out.println("bit b2 setado em 1");
+                    }
+                }
+            }
         }
     }
 }
