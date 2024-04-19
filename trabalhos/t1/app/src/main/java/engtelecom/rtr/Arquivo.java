fdd001b6e32358e8b16869b4afb958b18c701b08
rafafonsek <irla.r@aluno.ifsc.edu.br>
2024-04-18 21:13:38 -0300

    adicionado classe Arquivo para fazer o tratamento do objeto arquivo e formatação para algo que possa se ler e trabalhar

--- trabalhos/t1/app/src/main/java/engtelecom/rtr/Arquivo.java
+++ trabalhos/t1/app/src/main/java/engtelecom/rtr/Arquivo.java
@@ -1,4 +1,41 @@
 package engtelecom.rtr;
 
+import java.util.Scanner;
+
 public class Arquivo {
+    private Scanner arquivo;
+    private String garbageString = "";
+    private String formattedString = "";
+
+    public Arquivo(Scanner arquivo) {
+        this.arquivo = arquivo;
+    }
+
+    private String openFile(){
+        String total = "";
+        while(arquivo.hasNextLine()) {
+            total += arquivo.nextLine();
+        }
+        return total;
+    }
+
+    public String formatString(){
+        String totalDeCaracteres = openFile();
+        System.out.println("total de caracteres antes" + totalDeCaracteres.length());
+
+        for (int c = 0; c < totalDeCaracteres.length(); c++){
+            if (totalDeCaracteres.charAt(c) == '0'){
+                formattedString += '0';
+            } else if (totalDeCaracteres.charAt(c) == '1'){
+                formattedString += '1';
+            } else{
+                garbageString += totalDeCaracteres.charAt(c);
+            }
+        }
+        //System.out.println(" Garbage String " + garbageString.length());
+        //System.out.println(garbageString);
+        //System.out.println("acabaou");
+        //System.out.println("total de caracteres após tratamento " + formattedString.length());
+        return formattedString;
+    }
 }
